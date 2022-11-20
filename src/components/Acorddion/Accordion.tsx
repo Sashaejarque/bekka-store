c0d9fbc49bff765f096aa7145a78e35b3008e31b
Sasha Ejarque <sasha.ejarque@poincenot.com>
2022-11-20 16:08:42 -0300

    Created discount function

---
+++ src/components/Acorddion/Accordion.tsx
@@ -0,0 +1,39 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Typography,
+} from "@mui/material";
+import React, { FC, PropsWithChildren } from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+interface Props {
+    children: React.ReactNode;
+    title: string;
+}
+const AccordionComponent: FC<Props> = ({ children, title }) => {
+  const [expanded, setExpanded] = React.useState<string | false>("panel1");
+
+  const handleChange =
+    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+    };
+  return (
+    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')} sx={{ width: '95%'}}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography sx={{ width: '33%', flexShrink: 0, fontSize: 20 }}>
+            {title}
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          {children}
+        </AccordionDetails>
+      </Accordion>
+  );
+};
+
+export default AccordionComponent;
--- src/components/DrawerCart/DrawerCart.tsx
+++ src/components/DrawerCart/DrawerCart.tsx
@@ -1,10 +1,20 @@
-import React, { FC, useMemo } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { FC, useEffect, useMemo, useState } from "react";
 import Drawer from "@mui/material/Drawer";
-import { Divider, Grid, IconButton, Typography } from "@mui/material";
+import {
+  Accordion,
+  Button,
+  Divider,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import useResponsiveScreen from "../../hooks/useResponsiveScreen";
 import CardCart from "../CardCart/CardCart";
 import { useShoppingCart } from "../../context/ShoppingCartContext";
+import AccordionComponent from "../Acorddion/Accordion";
 
 interface Props {
   open: boolean;
@@ -15,12 +25,23 @@ const styles = {
     paddingLeft: 1.3,
   },
   divider: {
-    width: "92%",
+    width: "100%",
     marginTop: 2,
+  },
+  button: {
+    width: "90%",
+    backgroundColor: "black",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "orange",
+    },
   },
 };
 const DrawerCart: FC<Props> = ({ open, onClose }) => {
   const { cart } = useShoppingCart();
+  const [inputDiscount, setInputDiscount] = useState("");
+  const [isDiscountApplied, setIsDiscountApplied] = useState(0);
+  const [total, setTotal] = useState(0);
   const { width, height } = useResponsiveScreen();
   const responsiveWidth = useMemo(
     () => (width <= 800 ? width : width * 0.4),
@@ -28,7 +49,7 @@ const DrawerCart: FC<Props> = ({ open, onClose }) => {
   );
   const responsiveHeight = useMemo(() => height, [height]);
 
-  const totalCart = useMemo(() => {
+  /* const totalCart = useMemo(() => {
     return cart.reduce((acc, item) => {
       if (item.quantity != 0) {
         return acc + item.price * item.quantity;
@@ -36,7 +57,40 @@ const DrawerCart: FC<Props> = ({ open, onClose }) => {
         return acc + item.price;
       }
     }, 0);
-  }, [cart]);
+  }, [cart]); */
+  const reacalculateTotal = () => {
+    let totalCart = cart.reduce((acc, item) => {
+      if (item.quantity != 0) {
+        return acc + item.price * item.quantity;
+      } else {
+        return acc + item.price;
+      }
+    }, 0);
+    if (isDiscountApplied === 10) {
+      totalCart = totalCart - totalCart * 0.1;
+    }
+    if (isDiscountApplied === 20) {
+      totalCart = totalCart - totalCart * 0.2;
+    }
+    if (isDiscountApplied === 30) {
+      totalCart = totalCart - totalCart * 0.3;
+    }
+
+    return setTotal(totalCart);
+  };
+  useEffect(() => {
+    reacalculateTotal();
+  }, [total, isDiscountApplied, cart]);
+
+  const handleDiscount = () => {
+    if (inputDiscount === "DISC10") {
+      setIsDiscountApplied(10);
+    } else if (inputDiscount === "DISC20") {
+      setIsDiscountApplied(20);
+    } else if (inputDiscount === "DISC30") {
+      setIsDiscountApplied(30);
+    } else return alert("Invalid discount code");
+  };
 
   return (
     <Drawer
@@ -107,7 +161,50 @@ const DrawerCart: FC<Props> = ({ open, onClose }) => {
         {cart.length > 0 && (
           <>
             <Grid container sx={{ height: 140 }}>
-              <Grid container item xs={12} justifyContent="center">
+              <Grid
+                container
+                item
+                xs={12}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <AccordionComponent title="Discount">
+                  <Grid
+                    container
+                    item
+                    xs={12}
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Grid item xs={8}>
+                      <TextField
+                        variant="outlined"
+                        fullWidth
+                        size="small"
+                        onChange={(e) => setInputDiscount(e.target.value)}
+                      />
+                    </Grid>
+                    <Grid
+                      container
+                      item
+                      xs={4}
+                      justifyContent="center"
+                      alignItems="center"
+                    >
+                      <Button sx={styles.button} onClick={handleDiscount}>
+                        Apply
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </AccordionComponent>
+              </Grid>
+              <Grid
+                container
+                item
+                xs={12}
+                justifyContent="center"
+                alignItems="center"
+              >
                 <Divider sx={[styles.divider, { marginBottom: 2 }]} />
               </Grid>
               <Grid
@@ -119,11 +216,24 @@ const DrawerCart: FC<Props> = ({ open, onClose }) => {
                 ml={6}
                 mb={2}
               >
-                <Typography variant="h5" sx={styles.title}>
+                {isDiscountApplied !== 0 && (
+                  <Grid item xs={12}>
+                    <Typography
+                      sx={{ fontSize: 14, fontWeight: "bold", marginBottom: 2 }}
+                      textAlign="center"
+                    >
+                      Descuento aplicado: {isDiscountApplied}%
+                    </Typography>
+                  </Grid>
+                )}
+                <Typography
+                  variant="h5"
+                  sx={[styles.title, { marginBottom: 2 }]}
+                >
                   Total:
                 </Typography>
-                <Typography variant="h5">
-                  ${totalCart.toFixed(2)}
+                <Typography variant="h5" sx={{ marginBottom: 2 }}>
+                  {total.toFixed(2)}
                 </Typography>
               </Grid>
             </Grid>
